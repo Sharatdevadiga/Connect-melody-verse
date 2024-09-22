4998dbdb481877c520675602f15047945416ae32
sharath <sharathdevadiga210@gmail.com>
2024-09-22 21:14:02 +0530

    fixed some bugs

--- Connectverse-API/index.js
+++ Connectverse-API/index.js
@@ -1,7 +1,12 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const connectDB = require("./config/db"); // Assuming you have a connectDB function
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./config/connect.js";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -12,11 +17,11 @@ app.use(cookieParser());
 // CONNECT TO DATABASE
 connectDB();
 
-// CORS Configuration
+// cors
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://connect-melody-verse.vercel.app",
+  "https://connect-melody-verse.vercel.app/",
 ];
 
 app.use(
@@ -28,15 +33,15 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    credentials: true,
   })
 );
 
-// Your routes here
-app.use("/api/v1/auth", require("./routes/authRoutes"));
+// ROUTES
+app.use("/api/v1/auth", authRouter);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
+// sTART THE SERVER
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("✅ Server started successfully on port", PORT);
 });
